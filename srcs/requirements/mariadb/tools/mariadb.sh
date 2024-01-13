5d85f8ceb3b2dbfda634c406f962a456d926b15a
3bduu <abdelilah.benlahbib@usmba.ac.ma>
2024-01-13 15:44:37 +0100

    FSH

--- srcs/requirements/mariadb/tools/mariadb.sh
+++ srcs/requirements/mariadb/tools/mariadb.sh
@@ -4,10 +4,6 @@ sed -i "s/127.0.0.1/0.0.0.0/" /etc/mysql/mariadb.conf.d/50-server.cnf
 
 service mariadb start
 
-if [ -d "/var/lib/mysql/$MYSQL_DATABASE" ];then 
-	echo "Database already exists"
-else
-
 mysql_secure_installation << EOF
 
 Y
@@ -19,13 +15,12 @@ Y
 Y
 EOF
 
-echo "GRANT ALL ON *.* TO 'root'@'%' IDENTIFIED BY '$MYSQL_ROOT_PW'; FLUSH PRIVILEGES;" | mysql -uroot
 echo "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE ;" | mysql -uroot
+echo "CREATE USER IF NOT EXISTS '$MYSQL_ROOT'@'%' IDENTIFIED BY '$MYSQL_ROOT_PW' ;" | mysql -uroot
+echo "GRANT ALL PRIVILEGES ON *.* TO '$MYSQL_ROOT'@'%';" | mysql -uroot
 echo "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD' ;" | mysql -uroot
-echo "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';" | mysql -uroot
+echo "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%';" | mysql -uroot
 echo "FLUSH PRIVILEGES;" | mysql -uroot
-		
-fi
 
 service mariadb stop
 
